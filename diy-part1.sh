c1e10e60967cb9e637f33724b29518d715ccf55b
cqmylx <67597378+cqmylx@users.noreply.github.com>
2021-02-08 11:59:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-#git clone https://github.com/godros/luci-app-godproxy.git ./package/lean/luci-app-godproxy
+git clone https://github.com/godros/luci-app-godproxy.git ./package/lean/luci-app-godproxy
