3de6563b2bafa0d84aa1b811c42d3bb89c60dfde
cqmylx <67597378+cqmylx@users.noreply.github.com>
2023-03-21 09:15:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,3 +19,17 @@ sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/emortal/default-settings/files/99-defa
 #sed -i "s/auto/'zh_cn'/g" feeds/luci/modules/luci-base/root/etc/config/luci
 #sed -i "/config internal languages/a\option zh_cn '简体中文 (Simplified Chinese)'\noption en  'English'" feeds/luci/modules/luci-base/root/etc/config/luci
 #sed -i "s/config internal languages/config internal languages\noption zh_cn '简体中文 (Simplified Chinese)'\noption en  'English'/g" feeds/luci/modules/luci-base/root/etc/config/luci
+
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
+rm -rf ./package/diy-ziyong/theme
+rm -rf ./package/diy-ziyong/luci-app-wrtbwmon-zh
+rm -rf ./package/diy-ziyong/wrtbwmon
+# rm -rf ./package/diy-ziyong/adguardhome
+rm -rf ./feeds/packages/net/adguardhome
+rm -rf ./feeds/packages/net/smartdns
+rm -rf ./feeds/packages/net/mosdns
+rm -rf ./feeds/luci/applications/luci-app-passwall
+rm -rf ./feeds/luci/applications/luci-app-ssr-plus
+rm -rf ./feeds/luci/applications/luci-app-openclash
+rm -rf ./feeds/luci/applications/luci-app-smartdns
+# rm -rf ./feeds/luci/applications/luci-app-adbyby-plus
