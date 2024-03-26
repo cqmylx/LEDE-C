87efbd0a3bc421c69371d30f10a9611dada4d21e
cqmylx <67597378+cqmylx@users.noreply.github.com>
2024-03-27 00:25:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,9 +21,9 @@
 #git clone https://github.com/zijieKwok/istoreos-nas-packages.git package/istoreos-nas-packages
 git clone https://github.com/animegasan/luci-app-quickstart package/luci-app-quickstart
 git clone https://github.com/linkease/istore.git package/istoreos
-rm -rf feeds/applications/luci-app-passwall
+#rm -rf feeds/applications/luci-app-passwall
 #git clone -b packages https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+#git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 #rm -rf feeds/net/packages/smartdns
 #git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 git clone https://github.com/zxl78585/luci-app-filetransfer.git package/luci-app-filetransfer
