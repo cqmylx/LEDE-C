b23ae5220a6cbb3bb574d33bc98db066a50f59aa
cqmylx <67597378+cqmylx@users.noreply.github.com>
2024-03-28 00:36:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,9 +18,9 @@
 #sed -i '$a src-git istore https://github.com/linkease/istore' feeds.conf.default
 
 #git clone https://github.com/fw876/helloworld.git package/ssr
-git clone https://github.com/zijieKwok/istoreos-nas-packages.git package/istoreos-nas-packages
+#git clone https://github.com/zijieKwok/istoreos-nas-packages.git package/istoreos-nas-packages
 #git clone https://github.com/animegasan/luci-app-quickstart package/luci-app-quickstart
-git clone https://github.com/linkease/istore.git package/istoreos
+#git clone https://github.com/linkease/istore.git package/istoreos
 rm -rf feeds/applications/luci-app-passwall
 git clone -b packages https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
 #git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
