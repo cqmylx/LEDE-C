072e6be6ab3f443f69f9a8eb10d49f0f8be88b35
cqmylx <67597378+cqmylx@users.noreply.github.com>
2024-12-09 16:42:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,13 +13,14 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+rm -rf feeds/luci/applications/luci-app-passwall
 # 添加PassWall插件
 #echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 #echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git istore https://github.com/linkease/istore;main' feeds.conf.default
+#sed -i '$a src-git istore https://github.com/linkease/istore;main' feeds.conf.default
 #git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 #git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
@@ -27,7 +28,6 @@ sed -i '$a src-git istore https://github.com/linkease/istore;main' feeds.conf.de
 #git clone https://github.com/zijieKwok/istoreos-nas-packages.git package/istoreos-nas-packages
 #git clone https://github.com/animegasan/luci-app-quickstart package/luci-app-quickstart
 #git clone https://github.com/linkease/istore.git package/istoreos
-#rm -rf feeds/luci/applications/luci-app-passwall
 #git clone -b packages https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
 #git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 #git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
